37082df37a0c865a368d11c7bd521c6047576da5
Guvanch Amanov <guvanch.amanov@skystart.team>
2024-01-02 20:15:15 +0300

    Initial Commit

---
+++ onboarding_test/onboarding_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  onboarding_testApp.swift
+//  onboarding_test
+//
+//  Created by Guvanch Amanov on 2.01.24.
+//
+
+import SwiftUI
+
+@main
+struct onboarding_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
